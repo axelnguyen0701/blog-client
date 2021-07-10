a0497fa86d78430c72ea3d392d71adea680f0179
hieunguyen <axelnguyen0701@gmail.com>
2021-07-10 16:06:26 -0600

    Fix api address (2)

--- src/apis/client.js
+++ src/apis/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://axel-blog-api.herokuapp.com/posts",
+  baseURL: "https://axel-blog-api.herokuapp.com/",
   headers: {
     authorization: "bearer " + localStorage.getItem("user_token"),
   },

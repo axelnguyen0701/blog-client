a830c81fe0cba522045595072f6725df00ddcd99
Hieu Nguyen <axelnguyen0701@gmail.com>
2023-01-05 20:14:00 -0700

    change base url

--- src/apis/client.js
+++ src/apis/client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://axel-blog-api.herokuapp.com/",
-  headers: {
-    authorization: "bearer " + localStorage.getItem("user_token"),
-  },
+    baseURL: "https://blog-api-opbw.onrender.com/",
+    headers: {
+        authorization: "bearer " + localStorage.getItem("user_token"),
+    },
 });
